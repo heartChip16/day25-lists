33a875f0d1dd3704894e03a093966f2779c7d07d
Ingrid E. Torres <ingrid.escabal@gmail.com>
2022-11-23 16:25:59 +0800

    shuffle pizza toppings

--- src/PizzaToppings.jsx
+++ src/PizzaToppings.jsx
@@ -1,5 +1,47 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+// const pizzaToppings = [
+//     {
+//         id: "o",
+//         topping: "onion",
+//     },
+//     {
+//         id: "m",
+//         topping: "margherita",
+//     },
+//     {
+//         id: "j",
+//         topping: "jalapeno",
+//     },
+//     {
+//         id: "p",
+//         topping: "pineapple",
+//     },
+// ];
+
+
+// function PizzaToppings() {
+//     const [toppings, setToppings] = useState(pizzaToppings);
+//     function removeTopping(topping) {
+//         setToppings(toppings.filter(i => i !== topping))
+//     }
+//     return (<ul>
+//         {
+//             toppings.map((item) => (
+//                 <li key={item.id}>
+//                     <label htmlFor={item.topping}>{item.topping}</label>
+//                     <input type="text" name={item.topping} id={item.id} defaultValue={item.topping} />
+//                     <button onClick={() => removeTopping(item)}>X</button>
+//                 </li>
+//             )
+//             )
+//         }
+//     </ul>)
+// }
+
+// export default PizzaToppings
+
 
 const pizzaToppings = [
     {
@@ -20,19 +62,33 @@ const pizzaToppings = [
     },
 ];
 
-
+function shuffle(originalArray) {
+    const shuffledArray = [...originalArray];
+    for (let index = shuffledArray.length - 1; index > 0; index--) {
+        let randomIndex = Math.floor(Math.random() * (index + 1));
+        [shuffledArray[index], shuffledArray[randomIndex]] = [shuffledArray[randomIndex], shuffledArray[index]];
+    }
+    return shuffledArray;
+}
 function PizzaToppings() {
     const [toppings, setToppings] = useState(pizzaToppings);
     function removeTopping(topping) {
         setToppings(toppings.filter(i => i !== topping))
     }
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setToppings(shuffle(toppings));
+        }, 2000);
+        return () => {
+            clearInterval(interval);
+        }
+    }, [])
+
     return (<ul>
         {
-            toppings.map((item) => (
+            toppings?.map((item) => (
                 <li key={item.id}>
-                    <label htmlFor={item.topping}>{item.topping}</label>
-                    <input type="text" name={item.topping} id={item.id} defaultValue={item.topping} />
-                    <button onClick={() => removeTopping(item)}>X</button>
+                    <input type="text" value={item.topping} />
                 </li>
             )
             )
